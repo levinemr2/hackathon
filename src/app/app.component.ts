f44954135e838758f132105bba05d2aea1675289
Molly Levine <mlevine@PMIDSD.com>
2022-03-17 14:25:37 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ChatService} from './chat.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackathon';
+
+ newMessage = '';
+  messageList: string[] = [];
+ 
+
+  constructor(private chatService: ChatService){}
+
+  ngOnInit(){
+    this.chatService.getNewMessage().subscribe((message: string) => {
+      this.messageList.push(message);
+    })
+  }
+
+  sendMessage(){
+    
+    this.chatService.sendMessage(this.newMessage);
+    this.newMessage = '';
+    console.log('onclick works')
+  }
 }
